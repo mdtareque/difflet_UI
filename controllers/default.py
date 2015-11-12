af781c7d98f9466ea085cbd36d626118b8724841
mdtareque <mohdtarequekhan+gh@gmail.com>
2015-11-13 02:12:10 +0530

    kickstart difflet project
    * Added initial web2py application
    * Basic model created
    * Basic controller to display difference between two entities done
    * simple ui prototype created

---
+++ controllers/default.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# this file is released under public domain and you can use without limitations
+
+#########################################################################
+## This is a sample controller
+## - index is the default action of any application
+## - user is required for authentication and authorization
+## - download is for downloading files uploaded in the db (does streaming)
+#########################################################################
+
+def index():
+    """
+    example action using the internationalization operator T and flash
+    rendered by views/default/index.html or views/generic.html
+
+    if you need a simple wiki simply replace the two lines below with:
+    return auth.wiki()
+    """
+    response.flash = T("Hello World")
+    return dict(message=T('Welcome to web2py!'))
+
+def difflet():
+    e1 = request.vars['e1']
+    e2 = request.vars['e2']
+    e1id=(db(db.entity.name==e1).select())[0]['id']
+    e2id=(db(db.entity.name==e2).select())[0]['id']
+
+    s1="select name, summary from diff_point, listing where listing.diff_point == diff_point.id and listing.entity==%d  order by diff_point.name" % e1id
+    s1_res = db.executesql(s1)
+    s2="select name, summary from diff_point, listing where listing.diff_point == diff_point.id and listing.entity==%d  order by diff_point.name" % e2id
+    s2_res = db.executesql(s2)
+    return locals()
+
+def user():
+    """
+    exposes:
+    http://..../[app]/default/user/login
+    http://..../[app]/default/user/logout
+    http://..../[app]/default/user/register
+    http://..../[app]/default/user/profile
+    http://..../[app]/default/user/retrieve_password
+    http://..../[app]/default/user/change_password
+    http://..../[app]/default/user/manage_users (requires membership in
+    http://..../[app]/default/user/bulk_register
+    use @auth.requires_login()
+        @auth.requires_membership('group name')
+        @auth.requires_permission('read','table name',record_id)
+    to decorate functions that need access control
+    """
+    return dict(form=auth())
+
+
+@cache.action()
+def download():
+    """
+    allows downloading of uploaded files
+    http://..../[app]/default/download/[filename]
+    """
+    return response.download(request, db)
+
+
+def call():
+    """
+    exposes services. for example:
+    http://..../[app]/default/call/jsonrpc
+    decorate with @services.jsonrpc the functions to expose
+    supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
+    """
+    return service()
---
+++ models/db_difflet.py.bak
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+db.define_table('category', Field('name'))
+#                , auth.signature)
+
+db.define_table('entity'
+               ,Field('category', 'reference category')
+               ,Field('name'))
+#               ,auth.signature)
+
+db.define_table('diff_point'
+               ,Field('name'))
+#               ,auth.signature)
+
+db.define_table('listing'
+               ,Field('entity', 'reference entity')
+               ,Field('diff_point', 'reference diff_point')
+               ,Field('summary', 'text'))
+#               ,auth.signature)
+
+# db.define_table('difflet'
+#                ,Field('entity1', 'reference entity')
+#                ,Field('entity2', 'reference entity')
+#                ,Field('score', default=0))
+# # db.define_table('vote'
+#                ,Field('difflet', 'reference difflet')
+#                ,Field('score', 'integer', default=0)
+#                ,auth.signature)
